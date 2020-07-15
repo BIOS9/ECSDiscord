a2084a37ec7024d5901c1d349916a87a60476b2f
NightFish <nightfish.net@gmail.com>
2020-07-16 02:47:41 +1200

    Add comments to channel restrictions.

--- ECSDiscord/Modules/Util/Util.cs
+++ ECSDiscord/Modules/Util/Util.cs
@@ -16,14 +16,15 @@ namespace ECSDiscord.Modules.Util
         /// </summary>
         public static bool CheckConfigChannel(this SocketCommandContext context, string category, IConfigurationRoot config)
         {
-            IConfigurationSection section = config.GetSection("forcedChannels").GetSection(category);
+            IConfigurationSection section = config.GetSection("forcedChannels").GetSection(category); // Get command category from config
 
-            List<ulong> allowedChannels = new List<ulong>();
+            List<ulong> allowedChannels = new List<ulong>(); // List of allowed channels to show user
             foreach (IConfigurationSection child in section.GetChildren())
             {
-                if (string.IsNullOrWhiteSpace(child.Value))
+                if (string.IsNullOrWhiteSpace(child.Value)) // Ignore null or empty channels
                     continue;
 
+                // Attempt to convert configured channel to a ulong
                 ulong allowedChannel;
                 if (!ulong.TryParse(child.Value, out allowedChannel))
                 {
@@ -33,17 +34,20 @@ namespace ECSDiscord.Modules.Util
 
                 allowedChannels.Add(allowedChannel);
 
-                if (context.Channel.Id == allowedChannel)
+                if (context.Channel.Id == allowedChannel) // Check if channels match
                     return true;
             }
 
-            if (allowedChannels.Count == 0)
+            if (allowedChannels.Count == 0) // If no channels are allowed, assume unconfigured so allow all.
                 return true;
 
+            // Log and send message to user.
             context.Channel.SendMessageAsync("Sorry, that command can only be used in " +
                 allowedChannels.Select(x => MentionUtils.MentionChannel(x)).Aggregate((x, y) => $"{x}, {y}"));
             Log.Information("User {user} was disallowed access to command because execution is not allowed in channel {channel}",
                 context.User.ToString(), context.Channel.Name);
+
+            // Disallow access.
             return false;     
         }
     }
