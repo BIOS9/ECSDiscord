34be60dd1c4b1f97954eef259ee0763fc972075d
NightFish <nightfish.net@gmail.com>
2020-07-16 16:32:31 +1200

    Add course validation

--- ECSDiscord/Modules/EnrollmentsModule.cs
+++ ECSDiscord/Modules/EnrollmentsModule.cs
@@ -1,6 +1,10 @@
 ﻿using Discord.Commands;
 using ECSDiscord.Modules.Util;
+using ECSDiscord.Services;
 using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ECSDiscord.BotModules
@@ -9,6 +13,8 @@ namespace ECSDiscord.BotModules
     [RequireContext(ContextType.Guild)]
     public class EnrollmentsModule : ModuleBase<SocketCommandContext>
     {
+        
+
         private readonly IConfigurationRoot _config;
 
         public EnrollmentsModule(IConfigurationRoot config)
@@ -21,7 +27,18 @@ namespace ECSDiscord.BotModules
         [Summary("Join a uni course channel.")]
         public async Task JoinAsync(params string[] courses)
         {
-            if (!Context.CheckConfigChannel("enrollments", _config)) return; // Ensure command is only executed in allowed channels
+            // Ensure command is only executed in allowed channels
+            if (!Context.CheckConfigChannel("enrollments", _config)) return;
+
+            // Ensure course list is valid
+            if (!checkCourses(courses, true, out string errorMessage, out ISet<string> formattedCourses))
+            {
+                await ReplyAsync(errorMessage);
+                return;
+            }
+            
+
+            await ReplyAsync(string.Join(", ", formattedCourses));
         }
 
         [Command("leave")]
@@ -29,7 +46,18 @@ namespace ECSDiscord.BotModules
         [Summary("Leave a uni course channel.")]
         public async Task LeaveAsync(params string[] courses)
         {
-            if (!Context.CheckConfigChannel("enrollments", _config)) return; // Ensure command is only executed in allowed channels
+            // Ensure command is only executed in allowed channels
+            if (!Context.CheckConfigChannel("enrollments", _config)) return;
+
+            // Ensure course list is valid
+            if (!checkCourses(courses, true, out string errorMessage, out ISet<string> formattedCourses))
+            {
+                await ReplyAsync(errorMessage);
+                return;
+            }
+
+
+            await ReplyAsync(string.Join(", ", formattedCourses));
         }
 
         [Command("togglecourse")]
@@ -37,7 +65,18 @@ namespace ECSDiscord.BotModules
         [Summary("Join or leave a uni course channel.")]
         public async Task ToggleCourseAsync(params string[] courses)
         {
-            if (!Context.CheckConfigChannel("enrollments", _config)) return; // Ensure command is only executed in allowed channels
+            // Ensure command is only executed in allowed channels
+            if (!Context.CheckConfigChannel("enrollments", _config)) return;
+
+            // Ensure course list is valid
+            if (!checkCourses(courses, false, out string errorMessage, out ISet<string> formattedCourses))
+            {
+                await ReplyAsync(errorMessage);
+                return;
+            }
+
+
+            await ReplyAsync(string.Join(", ", formattedCourses));
         }
 
         [Command("courses")]
@@ -47,6 +86,55 @@ namespace ECSDiscord.BotModules
         {
             if (!Context.CheckConfigChannel("enrollments", _config)) return; // Ensure command is only executed in allowed channels
             //ReplyAsync
+        }
+
+        private bool checkCourses(string[] courses, bool ignoreDuplicates, out string errorMessage, out ISet<string> formattedCourses)
+        {
+            // Ensure courses are provided by user
+            if (courses == null || courses.Length == 0)
+            {
+                errorMessage = "Please specify one or more courses to join (separated by spaces) e.g\n```" +
+                      _config["prefix"] + "join comp102 engr101```";
+                formattedCourses = null;
+                return false;
+            }
+
+            HashSet<string> distinctCourses = new HashSet<string>();
+            HashSet<string> duplicateCourses = new HashSet<string>();
+            HashSet<string> invalidFormatCourses = new HashSet<string>();
+            foreach (string course in courses)
+            {
+                string normalised = EnrollmentsService.NormaliseCourseName(course);
+
+                if (!EnrollmentsService.IsCourseValid(course)) // Ensure all courses are in a valid format
+                    invalidFormatCourses.Add('`' + course + '`');
+
+                if (!string.IsNullOrEmpty(normalised) && !distinctCourses.Add(normalised)) // Enrusre there are no duplicate courses
+                    duplicateCourses.Add('`' + normalised + '`');
+            }
+            string error = "";
+            if (invalidFormatCourses.Count != 0) // Error invalid courses
+            {
+                string s = invalidFormatCourses.Count > 1 ? "s" : "";
+                string courseList = invalidFormatCourses.Aggregate((x, y) => $"{x}, {y}");
+                error += $"The following courses are not valid: {courseList}.\nPlease ensure the courses are in the format ABCD123 or ABCD-123 (case-insensitive)";
+            }
+            if (duplicateCourses.Count != 0 && !ignoreDuplicates) // Error duplicate courses
+            {
+                string s = duplicateCourses.Count > 1 ? "s" : "";
+                string courseList = duplicateCourses.Aggregate((x, y) => $"{x}, {y}");
+                error += $"\nDuplicate course{s} found: {courseList}.\nPlease ensure there are no duplicate course.";
+            }
+            if (invalidFormatCourses.Count != 0 || (duplicateCourses.Count != 0 && !ignoreDuplicates)) // Print then end if courses have duplicates or are invalid
+            {
+                errorMessage = error.Trim();
+                formattedCourses = null;
+                return false;
+            }
+
+            errorMessage = string.Empty;
+            formattedCourses = distinctCourses;
+            return true;
         }
     }
 }
--- ECSDiscord/Services/EnrollmentsService.cs
+++ ECSDiscord/Services/EnrollmentsService.cs
@@ -3,12 +3,15 @@ using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ECSDiscord.Services
 {
     public class EnrollmentsService
     {
+        private static readonly Regex CourseRegex = new Regex("([A-Za-z]{4})[ -_]?([0-9]{3})"); // Pattern for course names
+
         private readonly DiscordSocketClient _discord;
         private readonly IConfigurationRoot _config;
         
@@ -18,6 +21,7 @@ namespace ECSDiscord.Services
             CourseNotExist,
             AlreadyJoined,
             AlreadyLeft,
+            Failure
         }
 
         public EnrollmentsService(DiscordSocketClient discord, IConfigurationRoot config)
@@ -28,17 +32,31 @@ namespace ECSDiscord.Services
 
         public async Task<EnrollmentResult> EnrollUser(string course, SocketGuildUser user)
         {
-
+            return EnrollmentResult.Failure;
         }
 
         public async Task<EnrollmentResult> DisenrollUser(string course, SocketGuildUser user)
         {
-
+            return EnrollmentResult.Failure;
         }
 
-        public async Task<List<string>> DisenrollUser(string course, SocketGuildUser user)
+        public async Task<List<string>> GetCourses(SocketGuildUser user)
         {
+            return null;
+        }
 
+        public static string NormaliseCourseName(string course)
+        {
+            Match match = CourseRegex.Match(course);
+            if (!match.Success)
+                return string.Empty;
+
+            return match.Groups[1].Value.ToUpper() + "-" + match.Groups[2].Value;
+        }
+
+        public static bool IsCourseValid(string course)
+        {
+            return CourseRegex.IsMatch(course);
         }
     }
 }
