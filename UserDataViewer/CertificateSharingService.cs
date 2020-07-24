c0b2a4337c8d78d3b126e3fab2851a43e0d48066
NightFish <nightfish.net@gmail.com>
2020-07-25 04:43:23 +1200

    Remove unused libs

--- UserDataViewer/CertificateSharingService.cs
+++ UserDataViewer/CertificateSharingService.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
-using System.Text;
 
 namespace UserDataViewer
 {
