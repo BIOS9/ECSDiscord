08c601025e052e087bd971e13eb6148dc1985d85
NightFish <nightfish.net@gmail.com>
2020-11-19 09:08:37 +1300

    Comment resource loader.

--- ComponentApplication/Components/Resources/IResourceLoader.cs
+++ ComponentApplication/Components/Resources/IResourceLoader.cs
@@ -7,8 +7,23 @@ namespace ComponentApplication.Components.Resources
 {
     public interface IResourceLoader
     {
+        /// <summary>
+        /// Load resource assemblies into the resource loader.
+        /// </summary>
         void LoadAssemblies();
+
+        /// <summary>
+        /// Get the assemblies that have been loaded into the resource loader.
+        /// </summary>
+        /// <returns>Collection of loaded assemblies.</returns>
         ICollection<Assembly> GetAssemblies();
+
+        /// <summary>
+        /// Assembly resolve event handler used to allow the <see cref="AppDomain"/> to load assemblies from the loader. 
+        /// </summary>
+        /// <param name="sender">Object that called the resolve callback.</param>
+        /// <param name="args">Resolve event arguments.</param>
+        /// <returns>Resource assembly.</returns>
         Assembly AssemblyResolve(object sender, ResolveEventArgs args);
     }
 }
--- ComponentApplication/Components/Resources/PluginResourceLoader.cs
+++ ComponentApplication/Components/Resources/PluginResourceLoader.cs
@@ -13,15 +13,23 @@ namespace ComponentApplication.Components.Resources
     /// </summary>
     internal class PluginResourceLoader : IResourceLoader
     {
-        private static readonly Regex versionRegex = new Regex("Version=(?<version>.+?), ", RegexOptions.Compiled);
-        protected static string GetVersionIndependentName(string fullAssemblyName, out string extractedVersion)
+        private static readonly Regex versionRegex = new Regex("Version=(?<version>.+?), ", RegexOptions.Compiled); // Regex for the version number in the assembly name.
+
+        private IDictionary<string, Assembly> _resourceAssemblies = new Dictionary<string, Assembly>();
+
+        /// <summary>
+        /// Get the full name of an assembly with the version information removed.
+        /// </summary>
+        /// <param name="fullAssemblyName">Full name of an assembly.</param>
+        /// <param name="extractedVersion">Version that was removed.</param>
+        /// <returns>Name string with version removed.</returns>
+        /// <remarks>Source Rocket.Unturned: https://github.com/RocketMod/Rocket.Unturned/blob/5b684f782678c740006c844a79d17a36d2babefe/Rocket.Unturned.Module/RocketUnturnedModule.cs#L146 </remarks>
+        private static string GetVersionIndependentName(string fullAssemblyName, out string extractedVersion)
         {
             var match = versionRegex.Match(fullAssemblyName);
             extractedVersion = match.Groups[1].Value;
             return versionRegex.Replace(fullAssemblyName, "");
         }
-
-        private IDictionary<string, Assembly> _resourceAssemblies = new Dictionary<string, Assembly>();
 
         public void LoadAssemblies()
         {
