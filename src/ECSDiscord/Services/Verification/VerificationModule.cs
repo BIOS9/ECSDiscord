c217cf4aeda564bf0b5808cc96d6e6ae1e03ea19
NightFish <NightFish.net@gmail.com>
2023-04-17 18:50:36 +1200

    Use statically typed options for verification service

---
+++ src/ECSDiscord/Services/Verification/VerificationModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Autofac;
+using ECSDiscord.Util;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace ECSDiscord.Services.Verification;
+
+public class VerificationModule : Module
+{
+    private readonly IConfiguration _configuration;
+
+    public VerificationModule(IConfiguration configuration)
+    {
+        _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+    }
+    
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<VerificationService>()
+            .AsSelf()
+            .As<IHostedService>()
+            .SingleInstance();
+        builder.ConfigureWithValidation<VerificationOptions>(
+            _configuration.GetExistingSectionOrThrow(VerificationOptions.Name));
+        builder.AddFluentValidator<VerificationOptions, VerificationOptionsValidation>();
+    }
+}
---
+++ src/ECSDiscord/Util/FluentValidationOptions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.Extensions.Options;
+
+namespace ECSDiscord.Util;
+
+public class FluentValidationOptions<TOptions> 
+    : IValidateOptions<TOptions> where TOptions : class
+{
+    private readonly IValidator<TOptions> _validator;
+    
+    public FluentValidationOptions(IValidator<TOptions> validator)
+    {
+        _validator = validator;
+    }
+
+    public ValidateOptionsResult Validate(string? name, TOptions options)
+    {
+        // Ensure options are provided to validate against
+        ArgumentNullException.ThrowIfNull(options);
+
+        // Run the validation
+        ValidationResult results = _validator.Validate(options);
+        if (results.IsValid)
+        {
+            // All good!
+            return ValidateOptionsResult.Success;
+        }
+
+        // Validation failed, so build the error message
+        string typeName = options.GetType().Name;
+        var errors = new List<string>();
+        foreach (var result in results.Errors)
+        {
+            errors.Add($"Fluent validation failed for '{typeName}.{result.PropertyName}' with the error: '{result.ErrorMessage}'.");
+        }
+
+        return ValidateOptionsResult.Fail(errors);
+    }
+}
