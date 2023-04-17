5bf11f429da9a4d074df50736a20c5d23cf36b7d
NightFish <NightFish.net@gmail.com>
2023-04-17 16:05:11 +1200

    Add SendGrid email service

---
+++ src/ECSDiscord/Services/Email/Sendgrid/SendGridModule.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using Microsoft.Extensions.Configuration;
+using System;
+using ECSDiscord.Util;
+
+namespace ECSDiscord.Services.Email.Sendgrid;
+
+internal class SendGridModule : Module
+{
+    private readonly IConfiguration _configuration;
+
+    public SendGridModule(IConfiguration configuration)
+    {
+        _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+    }
+
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<SendGridMailSender>()
+            .As<IMailSender>();
+        builder.ConfigureWithValidation<SendGridOptions>(
+            _configuration.GetExistingSectionOrThrow(SendGridOptions.Name));
+    }
+}
---
+++ src/ECSDiscord/Services/Email/Sendgrid/SendGridOptions.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECSDiscord.Services.Email.Sendgrid;
+
+public class SendGridOptions
+{
+    public static string Name => "SendGrid";
+
+    [Required(AllowEmptyStrings = false)]
+    public string Token { get; init; }
+    
+    [Required]
+    [Range(1, ulong.MaxValue)]
+    public ulong GuildId { get; init; }
+    
+    [StringLength(128)]
+    public string StatusText { get; init; } = string.Empty;
+    
+}
