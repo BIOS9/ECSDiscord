145be653156efddec7134cf6a2f5ba95b3607ca9
NightFish <NightFish.net@gmail.com>
2023-04-17 16:42:41 +1200

    Use statically typed config for prefix commands service

--- src/ECSDiscord/Services/PrefixCommands/PrefixCommandsModule.cs
+++ src/ECSDiscord/Services/PrefixCommands/PrefixCommandsModule.cs
@@ -1,15 +1,27 @@
-﻿using Autofac;
+﻿using System;
+using Autofac;
+using ECSDiscord.Util;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace ECSDiscord.Services.PrefixCommands;
 
 public class PrefixCommandsModule : Module
 {
+    private readonly IConfiguration _configuration;
+
+    public PrefixCommandsModule(IConfiguration configuration)
+    {
+        _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+    }
+    
     protected override void Load(ContainerBuilder builder)
     {
         builder.RegisterType<PrefixCommandsHandler>()
             .AsSelf()
             .As<IHostedService>()
             .SingleInstance();
+        builder.ConfigureWithValidation<PrefixCommandsOptions>(
+            _configuration.GetExistingSectionOrThrow(PrefixCommandsOptions.Name));
     }
 }
