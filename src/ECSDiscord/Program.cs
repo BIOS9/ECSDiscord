ff46e8bde60fc8da5e2fc146310949b5d6101a67
NightFish <NightFish.net@gmail.com>
2023-04-17 19:13:09 +1200

    Refactor server message service into module

--- src/ECSDiscord/Program.cs
+++ src/ECSDiscord/Program.cs
@@ -8,6 +8,7 @@ using ECSDiscord.Services.Bot;
 using ECSDiscord.Services.Email.Sendgrid;
 using ECSDiscord.Services.Enrollments;
 using ECSDiscord.Services.PrefixCommands;
+using ECSDiscord.Services.ServerMessages;
 using ECSDiscord.Services.Storage;
 using ECSDiscord.Services.Translations;
 using ECSDiscord.Services.Verification;
@@ -26,7 +27,6 @@ await Host.CreateDefaultBuilder(args)
     {
         // all very ugly right now, will clean soon
         builder.RegisterType<ECSDiscord.Services.CourseService>().AsSelf().As<IHostedService>().SingleInstance();
-        builder.RegisterType<ECSDiscord.Services.ServerMessageService>().AsSelf().As<IHostedService>().SingleInstance();
 
         builder.RegisterModule(new BotModule(context.Configuration));
         builder.RegisterModule(new StorageModule(context.Configuration));
@@ -35,6 +35,7 @@ await Host.CreateDefaultBuilder(args)
         builder.RegisterModule(new VerificationModule(context.Configuration));
         builder.RegisterModule(new PrefixCommandsModule(context.Configuration));
         builder.RegisterModule<SlashCommandsModule>();
+        builder.RegisterModule<ServerMessagesModule>();
         builder.RegisterModule<TranslationsModule>();
     })
     .Build()
---
+++ src/ECSDiscord/Services/ServerMessages/ServerMessagesModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using Microsoft.Extensions.Hosting;
+
+namespace ECSDiscord.Services.ServerMessages;
+
+public class ServerMessagesModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<ServerMessageService>()
+            .AsSelf()
+            .As<IHostedService>()
+            .SingleInstance();
+    }
+}
