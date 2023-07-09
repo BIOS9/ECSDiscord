062c1251f308c3c029a26ac928a6b10be309d90f
NightFish <NightFish.net@gmail.com>
2023-07-09 19:57:26 +1200

    Implement course reset for current channel.

--- src/ECSDiscord/Services/SlashCommands/Commands/ResetCourseCommand.cs
+++ src/ECSDiscord/Services/SlashCommands/Commands/ResetCourseCommand.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using Discord;
+using Discord.WebSocket;
+using ECSDiscord.Services.Bot;
+using ECSDiscord.Util;
 
 namespace ECSDiscord.Services.SlashCommands.Commands;
 
 public class ResetCourseCommand : ISlashCommand
 {
-    public ResetCourseCommand()
+    private readonly DiscordBot _discordBot;
+    private readonly CourseService _courseService;
+    private readonly EnrollmentsService _enrollmentsService;
+    
+    public ResetCourseCommand(DiscordBot discordBot, CourseService courseService, EnrollmentsService enrollmentsService)
     {
-        
+        _discordBot = discordBot ?? throw new ArgumentNullException(nameof(discordBot));
+        _courseService = courseService ?? throw new ArgumentNullException(nameof(courseService));
+        _enrollmentsService = enrollmentsService ?? throw new ArgumentNullException(nameof(enrollmentsService));
     }
 
     public string Name => "resetcourse";
@@ -58,21 +68,60 @@ public class ResetCourseCommand : ISlashCommand
 
     private async Task ExecuteCurrentAsync(ISlashCommandInteraction command)
     {
-        var builder = new ComponentBuilder()
-            .WithRows(new[]
-            {
-                new ActionRowBuilder()
-                    .WithButton("Confirm", "confirm", ButtonStyle.Danger)
-                    .WithButton("Cancel", "cancel", ButtonStyle.Secondary)
-            });
+        var channel = await _discordBot.DiscordClient
+            .GetChannelAsync(command.ChannelId ?? throw new ArgumentNullException("ChannelId"));
+        if (!await _courseService.CourseExists(channel.Name))
+        {
+            await command.RespondAsync(":no_entry_sign:  Current channel is not a course channel.", ephemeral: true);
+            return;
+        }
         
-        await command.RespondAsync("##  :warning: Are you sure you want to reset the current channel?  :warning:\n" +
+        string confirmId = "coursereset-confirm" + StringExtensions.RandomString(50);
+        string cancelId = "coursereset-cancel" + StringExtensions.RandomString(50);
+
+        ComponentBuilder getButtonBuilder(bool disabled)
+        {
+            return new ComponentBuilder()
+                .WithRows(new[]
+                {
+                    new ActionRowBuilder()
+                        .WithButton("Confirm", confirmId, ButtonStyle.Danger, disabled: disabled)
+                        .WithButton("Cancel", cancelId, ButtonStyle.Secondary, disabled: disabled)
+                });
+        }
+
+        async Task ButtonEventHandler(SocketMessageComponent component)
+        {
+            if (component.Data.CustomId == confirmId)
+            {
+                _discordBot.DiscordClient.ButtonExecuted -= ButtonEventHandler;
+                await component.UpdateAsync(properties =>
+                {
+                    properties.Content = "## :white_check_mark:  Channel reset in progress.";
+                    properties.Components = null;
+                });
+                await ResetCourseChannel((SocketTextChannel)await component.GetChannelAsync());
+            }
+            else if (component.Data.CustomId == cancelId)
+            {
+                _discordBot.DiscordClient.ButtonExecuted -= ButtonEventHandler;
+                await component.UpdateAsync(properties =>
+                {
+                    properties.Content = "## :no_entry_sign:  Channel reset cancelled.";
+                    properties.Components = null;
+                });
+            }
+        }
+
+        _discordBot.DiscordClient.ButtonExecuted += ButtonEventHandler;
+        
+        await command.RespondAsync("## :warning:  Are you sure you want to reset the current channel?  :warning:\n" +
                                    "This means all of the following will be **__deleted__**:\n" +
                                    "* messages\n" +
                                    "* attachments\n" +
                                    "* images\n" +
                                    "* threads\n" +
-                                   "* pins", components: builder.Build(), ephemeral: true);
+                                   "* pins", components: getButtonBuilder(false).Build(), ephemeral: true);
     }
     
     private async Task ExecuteSelectAsync(ISlashCommandInteraction command)
@@ -94,4 +143,13 @@ public class ResetCourseCommand : ISlashCommand
             });
         await command.RespondAsync("Please choose which channels you want to reset.", components: builder.Build(), ephemeral: true);
     }
-}
+
+    private async Task ResetCourseChannel(SocketTextChannel channel)
+    {
+        var members = await _enrollmentsService.GetCourseMembers(channel.Name);
+        await _courseService.RemoveCourseAsync(channel.Name);
+        await channel.DeleteAsync();
+        await _courseService.CreateCourseAsync(channel.Name);
+        await _enrollmentsService.AddCourseMembers(channel.Name, members.Select(x => x.Id).ToList());
+    }
+} 
