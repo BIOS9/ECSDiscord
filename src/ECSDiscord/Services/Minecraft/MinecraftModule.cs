c4ad29bad77051157cec1c0aff3b8490e05eaf86
BIOS9 <nightfish.net@gmail.com>
2025-03-29 00:34:40 +1300

    feat: add minecraft service

---
+++ src/ECSDiscord/Services/Minecraft/MinecraftModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using Microsoft.Extensions.Hosting;
+
+namespace ECSDiscord.Services.Minecraft;
+
+public class MinecraftModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<MinecraftService>()
+            .AsSelf()
+            .As<IHostedService>()
+            .SingleInstance();
+    }
+}
---
+++ src/ECSDiscord/Services/Minecraft/MinecraftService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using ECSDiscord.Services.Bot;
+using ECSDiscord.Services.Storage;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace ECSDiscord.Services;
+
+public class MinecraftService : IHostedService
+{
+    private readonly DiscordBot _discord;
+    private readonly StorageService _storage;
+
+    public MinecraftService(DiscordBot discordBot, StorageService storage)
+    {
+        Log.Debug("Minecraft service loading.");
+        _discord = discordBot;
+        _storage = storage;
+        Log.Debug("Minecraft service loaded.");
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public async Task<IEnumerable<MinecraftAccount>> GetAllMinecraftAccountsAsync()
+    {
+        var minecraftAccounts = await _storage.Minecraft.GetMinecraftAccountsAsync();
+        return await Task.WhenAll(minecraftAccounts.Select(x => MinecraftAccount.CreateFromStorageAsync(
+            x,
+            _discord.DiscordClient)));
+    }
+
+    public async Task<MinecraftAccount> CreateMinecraftAccountAsync(string minecraftUuid, IUser discordUser, bool isExternal)
+    {
+        await _storage.Minecraft.CreateMinecraftAccountAsync(new StorageService.MinecraftStorage.MinecraftAccount(
+            minecraftUuid,
+            discordUser.Id,
+            DateTimeOffset.Now,
+            isExternal));
+        return await MinecraftAccount.CreateFromStorageAsync(
+            await _storage.Minecraft.GetMinecraftAccountAsync(minecraftUuid),
+            _discord.DiscordClient);
+    }
+
+    public async Task DeleteMinecraftAccountAsync(string minecraftUuid)
+    {
+        await _storage.Minecraft.DeleteMinecraftAccountAsync(minecraftUuid);
+    }
+
+    public record MinecraftAccount(
+        string MinecraftUuid, 
+        IUser DiscordUser,
+        DateTimeOffset CreatedAt,
+        bool IsExternal)
+    {
+        public static async Task<MinecraftAccount> CreateFromStorageAsync(
+            StorageService.MinecraftStorage.MinecraftAccount minecraftAccount,
+            DiscordSocketClient discordClient)
+        {
+            IUser discordUser = discordClient.GetUser(minecraftAccount.DiscordId);
+            return new MinecraftAccount(
+                minecraftAccount.MinecraftUuid,
+                discordUser,
+                minecraftAccount.CreationTime,
+                minecraftAccount.IsExternal);
+        }
+    }
+}
