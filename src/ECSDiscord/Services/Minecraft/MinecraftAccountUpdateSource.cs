31a24327f9ab98c26ec62e6bd66a676cc5c67ff8
BIOS9 <nightfish.net@gmail.com>
2025-03-29 17:47:54 +1300

    feat: add long polling minecraft web api

---
+++ src/ECSDiscord/Services/Minecraft/MinecraftAccountUpdateSource.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ECSDiscord.Services.Minecraft;
+
+/**
+ * Concurrency helper to allow multiple tasks to wait for updates to Minecraft accounts.
+ */
+public class MinecraftAccountUpdateSource
+{
+    private readonly List<TaskCompletionSource<bool>> _waitingTasks;
+
+    public MinecraftAccountUpdateSource()
+    {
+        _waitingTasks = new List<TaskCompletionSource<bool>>();
+    }
+    
+    public void SignalUpdate()
+    {
+        lock (_waitingTasks)
+        {
+            foreach (var taskCompletionSource in _waitingTasks)
+            {
+                taskCompletionSource.TrySetResult(true);
+            }
+
+            _waitingTasks.Clear();
+        }
+    }
+    
+    public async Task WaitForUpdateAsync(CancellationToken ct)
+    {
+        var tcs = new TaskCompletionSource<bool>();
+        
+        lock (_waitingTasks)
+        {
+            _waitingTasks.Add(tcs);
+        }
+        
+        using (ct.Register(() => tcs.TrySetCanceled()))
+        {
+            await tcs.Task;
+            
+            if (tcs.Task.IsCanceled)
+            {
+                throw new OperationCanceledException("Operation was canceled.");
+            }
+        }
+    }
+}
---
+++ src/ECSDiscord/Services/Minecraft/MinecraftApiController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECSDiscord.Services.Minecraft;
+
+[ApiController]
+[Route("api/minecraft")]
+public class MinecraftApiController : ControllerBase
+{
+    private readonly MinecraftService _minecraftService;
+    private readonly MinecraftAccountUpdateSource _accountUpdateSource;
+
+    public MinecraftApiController(MinecraftService minecraftService, MinecraftAccountUpdateSource accountUpdateSource)
+    {
+        _minecraftService = minecraftService ?? throw new ArgumentNullException(nameof(minecraftService));
+        _accountUpdateSource = accountUpdateSource ?? throw new ArgumentNullException(nameof(accountUpdateSource));
+    }
+
+    [HttpGet("accounts")]
+    public async Task<IActionResult> GetAccounts(bool wait = false, int timeout = 1000)
+    {
+        if (wait)
+        {
+            try
+            {
+                var cts = new CancellationTokenSource(TimeSpan.FromMilliseconds(timeout));
+                await _accountUpdateSource.WaitForUpdateAsync(cts.Token);
+            }
+            catch (OperationCanceledException e)
+            {
+                // Do nothing if timed out.
+            }
+        }
+        var accounts = await _minecraftService.GetAllMinecraftAccountsAsync();
+        return Ok(accounts.Select(x => new { uuid = x.MinecraftUuid }));
+    }
+}
