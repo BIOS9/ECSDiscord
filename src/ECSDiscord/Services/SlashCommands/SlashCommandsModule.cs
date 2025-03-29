dca661ebc81b283bd71a7d02252a01077bbc58b1
BIOS9 <nightfish.net@gmail.com>
2025-03-29 14:48:24 +1300

    feat: implement working mc account verification

--- src/ECSDiscord/Services/SlashCommands/SlashCommandsModule.cs
+++ src/ECSDiscord/Services/SlashCommands/SlashCommandsModule.cs
@@ -12,14 +12,14 @@ public class SlashCommandsModule : Module
             .AsSelf()
             .As<IHostedService>()
             .SingleInstance();
-        builder.RegisterType<VerifyCommand>().As<ISlashCommand>();
-        builder.RegisterType<JoinCommand>().As<ISlashCommand>();
-        builder.RegisterType<LeaveCommand>().As<ISlashCommand>();
-        builder.RegisterType<LeaveAllCommand>().As<ISlashCommand>();
-        builder.RegisterType<ListCoursesCommand>().As<ISlashCommand>();
-        builder.RegisterType<MyCoursesCommand>().As<ISlashCommand>();
-        builder.RegisterType<BotMessagesCommand>().As<ISlashCommand>();
-        builder.RegisterType<ResetCourseCommand>().As<ISlashCommand>();
+        // builder.RegisterType<VerifyCommand>().As<ISlashCommand>();
+        // builder.RegisterType<JoinCommand>().As<ISlashCommand>();
+        // builder.RegisterType<LeaveCommand>().As<ISlashCommand>();
+        // builder.RegisterType<LeaveAllCommand>().As<ISlashCommand>();
+        // builder.RegisterType<ListCoursesCommand>().As<ISlashCommand>();
+        // builder.RegisterType<MyCoursesCommand>().As<ISlashCommand>();
+        // builder.RegisterType<BotMessagesCommand>().As<ISlashCommand>();
+        // builder.RegisterType<ResetCourseCommand>().As<ISlashCommand>();
         builder.RegisterType<MinecraftCommand>().As<ISlashCommand>();
     }
 }
