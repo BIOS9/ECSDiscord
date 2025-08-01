92fcb44495b3f5664476e048101631aed67ce637
BIOS9 <nightfish.net@gmail.com>
2025-08-01 20:20:04 +1200

    feat: add smtp mail sender

---
+++ src/ECSDiscord/Services/Email/Smtp/SmtpModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Autofac;
+using ECSDiscord.Util;
+using Microsoft.Extensions.Configuration;
+
+namespace ECSDiscord.Services.Email.Smtp;
+
+internal class SmtpModule : Module
+{
+    private readonly IConfiguration _configuration;
+
+    public SmtpModule(IConfiguration configuration)
+    {
+        _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+    }
+
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<SmtpMailSender>()
+            .As<IMailSender>();
+        builder.ConfigureWithValidation<SmtpOptions>(
+            _configuration.GetExistingSectionOrThrow(SmtpOptions.Name));
+    }
+}
