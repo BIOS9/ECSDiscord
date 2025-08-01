b2101d77c280589e13bc26b0d421699c0f49264c
BIOS9 <nightfish.net@gmail.com>
2025-08-01 20:20:04 +1200

    feat: add smtp mail sender

---
+++ src/ECSDiscord/Services/Email/Smtp/SmtpMailSender.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace ECSDiscord.Services.Email.Smtp;
+
+public class SmtpMailSender : IMailSender
+{
+    private readonly ILogger<SmtpMailSender> _logger;
+    private readonly SmtpOptions _options;
+
+    public SmtpMailSender(IOptions<SmtpOptions> options, ILogger<SmtpMailSender> logger)
+    {
+        _options = options?.Value ?? throw new ArgumentNullException(nameof(options));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    public async Task<bool> SendMailAsync(string recipientAddress, string subject, string body, bool bodyIsHtml = false)
+    {
+        _logger.LogDebug("Sending email to {Recipient}", recipientAddress);
+        try
+        {
+            var mail = new MailMessage();
+            var client = new SmtpClient(_options.Server, _options.Port) //Port 8025, 587 and 25 can also be used.
+            {
+                Credentials = new NetworkCredential(_options.Username, _options.Password),
+                EnableSsl = _options.Ssl
+            };
+            mail.From = new MailAddress(_options.FromAddress, _options.FromName);
+            mail.To.Add(recipientAddress);
+            mail.Subject = subject;
+            mail.IsBodyHtml = bodyIsHtml;
+            mail.Body = body;
+
+            await client.SendMailAsync(mail);
+            _logger.LogInformation("Email sent successfully to {Recipient}", recipientAddress);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "SMTP error for recipient {}", recipientAddress);
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/ECSDiscord/Services/Email/Smtp/SmtpOptions.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECSDiscord.Services.Email.Smtp;
+
+public class SmtpOptions
+{
+    public static string Name => "Smtp";
+
+    [Required(AllowEmptyStrings = false)] public string Server { get; init; }
+    [Required(AllowEmptyStrings = false)] public int Port { get; init; }
+    [Required(AllowEmptyStrings = false)] public bool Ssl { get; init; }
+    [Required(AllowEmptyStrings = false)] public string Username { get; init; }
+    [Required(AllowEmptyStrings = false)] public string Password { get; init; }
+    [Required(AllowEmptyStrings = false)] public string FromAddress { get; init; }
+    [Required(AllowEmptyStrings = false)] public string FromName { get; init; }
+}
